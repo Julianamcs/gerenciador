f96d8331ae530ef060040467d00df44827662988
Juliana Maciel <julyanamcs@gmail.com>
2023-07-14 16:43:42 -0300

    remove

--- src/ber/com/alura/gerenciador/servlet/Banco.java
+++ src/ber/com/alura/gerenciador/servlet/Banco.java
@@ -1,34 +1,53 @@
 package ber.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequancial = 1; 
 	
-	//static {
-		//Empresa empresa = new Empresa();
-		//empresa.setNome("Alura");
-		///Empresa empresa1 = new Empresa();
-		///empresa1.setNome("Caelum");
-		//Empresa empresa2 = new Empresa();
-		//empresa2.setNome("PicPay");
-		//lista.add(empresa);
-		//lista.add(empresa1);
-		//lista.add(empresa2);
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		empresa.setId(chaveSequancial++);
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Caelum");
+		empresa1.setId(chaveSequancial++);
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("PicPay");
+		empresa2.setId(chaveSequancial++);
+		lista.add(empresa);
+		lista.add(empresa1);
+		lista.add(empresa2);
 		
-	//}
+	}
 
 	public void adiciona(Empresa empresa) {
 		// TODO Auto-generated method stub
-		lista.add(empresa);
+		empresa.setId(Banco.chaveSequancial++);
+		Banco.lista.add(empresa);
 		
 	}
 	
 	public List<Empresa> getEmpresas() {
 		return Banco.lista;
 		
+	}
+	
+	public void removeEmpresa(Integer id) { 
+
+	    Iterator<Empresa> it = lista.iterator();
+
+	    while(it.hasNext()) { 
+	        Empresa emp = it.next();
+
+	        if(emp.getId() == id ) {
+	            it.remove();
+	        }
+	    }
 	}
 
 }
